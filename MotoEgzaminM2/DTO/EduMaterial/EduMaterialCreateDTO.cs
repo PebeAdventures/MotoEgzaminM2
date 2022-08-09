a15272a72ffec6edfdbd3092494d413e5fcf965e
Piotr Bujak <piotr.bujak@motorolasolutions.com>
2022-08-09 19:04:32 +0200

    Add field validation to CreateDTOs

--- MotoEgzaminM2/DTO/EduMaterial/EduMaterialCreateDTO.cs
+++ MotoEgzaminM2/DTO/EduMaterial/EduMaterialCreateDTO.cs
@@ -1,6 +1,29 @@
-﻿namespace MotoEgzaminM2.DTO.EduMaterial
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotoEgzaminM2.DTO.EduMaterial
 {
     public class EduMaterialCreateDTO
     {
+        public int MaterialId { get; set; }
+
+        [Required]
+        [MaxLengthAttribute(30)]
+        public string Title { get; set; }
+        [Required]
+        [MaxLengthAttribute(250)]
+        public string Description { get; set; }
+        [Required]
+        [MaxLengthAttribute(250)]
+        public string Location { get; set; }
+        [Required]
+        [MaxLengthAttribute(5)]
+        public int AuthorId { get; set; }
+        [Required]
+        [MaxLengthAttribute(50)]
+        public int TypeId { get; set; }
+
+        [Required]
+        public string DateOfPublish { get; set; }
     }
 }
+
--- MotoEgzaminM2/DTO/EduMaterialReview/EduMaterialReviewCreateDTO.cs
+++ MotoEgzaminM2/DTO/EduMaterialReview/EduMaterialReviewCreateDTO.cs
@@ -1,6 +1,22 @@
-﻿namespace MotoEgzaminM2.DTO.EduMaterialReview
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotoEgzaminM2.DTO.EduMaterialReview
 {
     public class EduMaterialReviewCreateDTO
     {
+
+        public int ReviewId { get; set; }
+
+        [MaxLengthAttribute(50)]
+        public int? ReviewReferenceId { get; set; }
+
+        [Required]
+        [MaxLengthAttribute(250)]
+        public string ReviewText { get; set; }
+
+        [Required]
+        [Range(1, 10, ErrorMessage = "Score must be between 1 to 10")]
+        public int ReviewScore { get; set; }
     }
 }
+
